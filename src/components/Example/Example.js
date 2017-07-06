956598e24f6c3f728ddfa77433b4be15d17a92cd
FAIZ <faizahmadf@gmail.com>
2017-07-06 20:27:02 +0530

    Initial commit

---
+++ src/components/Example/Example.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Example extends React.Component {
+    render() {
+        return (
+            <div>This is Example Component!</div>
+        );
+    }
+}
+
+export default Example;
