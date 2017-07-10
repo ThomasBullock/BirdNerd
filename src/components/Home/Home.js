8eba80acd10d28391c9821ba43a8d5ab7dbfa49f
Thomas Bullock <motbollox@gmail.com>
2017-07-10 21:59:09 +1000

    Added topbar component and styling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import TopBar from '../TopBar';
 
 const Home = () => (
   <div>
-    <h2>Home</h2>
+  	<TopBar></TopBar>
+  	<main>
+  		<h2>This is the home page</h2>
+  	</main>
+
   </div>
 )
 
