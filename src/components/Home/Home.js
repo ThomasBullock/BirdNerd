4bc1ba8cbcada18d5b04b2c69e92733de79bafd2
Thomas Bullock <motbollox@gmail.com>
2017-07-15 22:55:22 +1000

    created birdcard component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import TopBar from '../TopBar';
+import BirdCard from '../BirdCard';
+
+import '../../styles/css/components/Home.css';
 
 const Home = () => (
   <div>
   	<TopBar></TopBar>
   	<main>
   		<h2>This is the home page</h2>
+  		<div className="container">
+	  		<BirdCard 
+	  			orientation="landscape"
+	  			img="http://www.australiananimallearningzone.com/wp-content/uploads/2012/03/Rainbow-Lorikeet-Pictures.jpg"
+	  			>
+	  		</BirdCard>
+	  		<BirdCard orientation="portrait"
+	  			img="http://www.blaize.net/wp-content/uploads/2011/02/Bird-3.jpg"
+	  		>
+	  		</BirdCard>	  		
+	  	</div>	
   	</main>
 
   </div>
