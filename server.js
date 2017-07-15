ba44fdfcd9d94ef898caa86e73032d2bc1551c59
FAIZ <faizahmadf@gmail.com>
2017-07-15 17:22:46 +0530

    Add changes to make nodejs use es6 syntax

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import apiRouter from './api';
 
 // import environmental variables from our variables.env file
@@ -11,7 +11,7 @@ require('dotenv').config({ path: 'variables.env' });
 const app = express();
 
 //set our port to either a predetermined port number if you have set it up, or 3001
-var port = process.env.API_PORT || 3001;
+const port = process.env.API_PORT || 3001;
 
 // Connect to our Database 
 mongoose.connect(process.env.DATABASE);
@@ -20,7 +20,11 @@ mongoose.connect(process.env.DATABASE);
 // This object will contain key-value pairs, where the value can be a string or array (when extended is false), or any type (when extended is true).
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true})); 
-app.use('/api', apiRouter);
+//app.use('/api', apiRouter);
+app.get('/', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+});
 
 //starts the server and listens for requests
 app.listen(port, function() {
