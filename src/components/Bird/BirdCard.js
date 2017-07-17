dd57228b22023f53a1d5a62d2f205d05cdba8227
Thomas Bullock <motbollox@gmail.com>
2017-07-17 15:31:38 +1000

    Styled buttons and added gradient

--- src/components/Bird/BirdCard.js
+++ src/components/Bird/BirdCard.js
@@ -1,4 +1,3 @@
-// import React from 'react';
 import React, { Component } from 'react';
 import '../../styles/css/components/BirdCard.css';
 
@@ -10,25 +9,29 @@ import IconBird from '../icons/IconBird';
 class BirdCard extends Component {
     //console.log(props.match.params);
 		render() {
+			const userImg = 'http://3.bp.blogspot.com/-dXOvZOVDhes/Ts99nTenjtI/AAAAAAAAA2A/It9Ymliw4t4/s1600/26.jpg';
 			const orientation = this.props.orientation;
 			return(
 				<div className={`birdcard birdcard--${orientation}`}>
 					<div className={`birdcard__photo birdcard__photo--${orientation}`}>
-						<img src={this.props.img}/>
+						<button style={{'backgroundImage': `${userImg}`}} className="birdcard__button birdcard__button--user"></button>
+						<img src={this.props.img} alt="bird name!!!"/>
 					</div>
 					<div className={`birdcard__stats birdcard__stats--${orientation}`}>
-						<span>
+						<button className="birdcard__button">
 							<IconLocation/>
-						</span>
-						<span>
+						</button>
+						<button className="birdcard__button">
 							<IconHeart/>
-						</span>
-						<span>
+							<span className="birdcard__number">3</span>
+						</button>
+						<button className="birdcard__button">
 							<IconBubble/>
-						</span>
-						<span>
+							<span className="birdcard__number">7</span>
+						</button>
+						<button className="birdcard__button">
 							<IconBird/>
-						</span>					
+						</button>					
 					</div>				
 				</div>
 			)
