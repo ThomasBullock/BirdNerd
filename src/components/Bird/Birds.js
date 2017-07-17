52f154fbd823570a5b49c826152aa8c7cd5b5244
Thomas Bullock <motbollox@gmail.com>
2017-07-17 12:56:09 +1000

    Added BirdCard component to development branch

--- src/components/Bird/Birds.js
+++ src/components/Bird/Birds.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import FullBird from './FullBird';
-import Bird from './Bird';
+import BirdCard from './BirdCard';
 import BirdForm from './BirdForm';
 
 const Birds = () => (
   <Switch>
     <Route exact path='/bird' component={FullBird}/>
     <Route exact path='/bird/new' component={BirdForm}/>
-    <Route path='/bird/:birdId' component={Bird}/>
+    <Route path='/bird/:birdId' component={BirdCard}/>
   </Switch>
 );
 
--- src/components/Bird/FullBird.js
+++ src/components/Bird/FullBird.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import '../../styles/css/components/FullBird.css';
+
+import BirdCard from './BirdCard';
 
 const FullBird = (props) => {
     return (
-        <div>This is FullBird page. Here the list of birds list will be shown.</div>
+    	<div className="container">
+        <BirdCard 
+	  			orientation="landscape"
+	  			img="http://www.australiananimallearningzone.com/wp-content/uploads/2012/03/Rainbow-Lorikeet-Pictures.jpg"
+	  			>
+	  		</BirdCard>
+	  		<BirdCard orientation="portrait"
+	  			img="http://www.blaize.net/wp-content/uploads/2011/02/Bird-3.jpg"
+	  		>
+	  		</BirdCard>
+	  		<BirdCard 
+	  			orientation="landscape"
+	  			img="https://upload.wikimedia.org/wikipedia/commons/7/72/Alcedo_azurea_-_Julatten.jpg"
+	  			>
+	  		</BirdCard>
+	  		<BirdCard 
+	  			orientation="portrait"
+	  			img="https://perthzoo.wa.gov.au/sites/default/files/animal/images/Southern%20Cassowary%20Image1.jpg"
+	  			>
+	  		</BirdCard>	  	  			  		
+	  	</div>	
     );
 }
 
