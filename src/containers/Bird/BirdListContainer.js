2f72430e63141819d4f265a405db421ba280514b
Thomas Bullock <motbollox@gmail.com>
2017-09-29 00:17:24 +1000

    reorganised birdlist to render birds by group(order)

--- src/containers/Bird/BirdListContainer.js
+++ src/containers/Bird/BirdListContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { birdGroups } from '../../clientHelpers';
 
 import {
   requestBirdList,
@@ -8,14 +9,41 @@ import {
 import BirdList from '../../components/Bird/BirdList';
 
 class BirdListContainer extends Component {
+	constructor(props) {
+		super(props)
+		
+		this.buildBirdList = this.buildBirdList.bind(this);
+	}
 	
 	componentDidMount() {
 		this.props.requestBirdList();
 	}
 	
+	buildBirdList(birds) {
+		
+		// console.log(birdGroups);
+		const groups = {};
+		for (var prop in birdGroups) {
+			// console.log(prop)
+			groups[prop] = [];
+		}
+		
+		birds.map( (bird) => {
+			const order = bird.order;
+			if(groups[`${bird.order}`]) {
+				groups[`${bird.order}`].push(bird);				
+			}
+		})
+		console.log(groups);
+		return groups;
+	}
+	
 	render() {
-		const birdList = (this.props.birdList.get('1')) ? this.props.birdList.get('1') : null;
-
+		const birdList = (this.props.birdList.get('1')) ? this.buildBirdList(this.props.birdList.get('1')) : null;
+		// if(birdList) {
+		// 	this.buildBirdList(birdList);	
+		// }
+		console.log(birdList)
 		return(
 			<div>
 			{birdList ? (
