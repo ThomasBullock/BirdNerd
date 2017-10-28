c2943702d3756c0406b3281e461b3f3ff01142cf
Thomas Bullock <motbollox@gmail.com>
2017-10-28 15:54:36 +1100

    Removed redundant ducks/sagas

--- src/ducks/index.js
+++ src/ducks/index.js
@@ -2,16 +2,12 @@ import { combineReducers } from 'redux-immutable';
 import { reducer as form } from 'redux-form/immutable';
 
 import bird from './bird';
-import birdList from './birdList';
-//import photo from './photo';
 import photos from './photos';
 import location from './location';
 import auth from './auth';
 
 const rootReducer = combineReducers({
     bird,
-    //birdList,
-    //photo,
     photos,
     location,
     auth,
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,5 @@
 import { fork } from 'redux-saga/effects';
 import birdSaga from './bird';
-import birdListSaga from './birdList';
-//import photoSaga from './photo';
 import photosSaga from './photos';
 
 import authSaga from './auth';
@@ -9,8 +7,6 @@ import authSaga from './auth';
 export default function* rootSaga() {
   yield [
     fork(birdSaga),
-    //fork(birdListSaga),
-    //fork(photoSaga),
     fork(photosSaga),    
     fork(authSaga),
   ];
