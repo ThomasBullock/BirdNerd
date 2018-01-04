54bf851c2d7255c808b8a68abe30f9a7a5cf94e1
Thomas Bullock <motbollox@gmail.com>
2018-01-04 15:04:02 +1100

    Added landing page styles and fixed generally styling issues

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -33,7 +33,7 @@ class Landing extends Component {
 				const aspectModifier = (aspect === 'Landscape') ? 'landing__grid-image landing__grid-image--aspect4x3' : 'landing__grid-image landing__grid-image--aspect3x4'
 				return(
 					<div className={aspectModifier}> 
-						<img src={image && cloudinaryUrlModify(image.split('/'), 'w_500') }/>
+						<img src={image && cloudinaryUrlModify(image.split('/'), 'w_300') }/>
 					</div>
 				)
 
@@ -48,7 +48,8 @@ class Landing extends Component {
 					{this.getImage('Portrait', 0)}
 					{this.getImage('Landscape', 1)}
 					{this.getImage('Landscape', 3)}
-					{this.getImage('Landscape', 8)}										
+					{this.getImage('Landscape', 8)}
+					{this.getImage('Landscape', 0)}																	
 				</div>
 				<div className="landing__hero-grid">
 					{this.getImage('Portrait', 1)}
@@ -59,8 +60,42 @@ class Landing extends Component {
 				</div>
 				<div className="landing__hero-grid">
 					{this.getImage('Landscape', 6)}
+					{this.getImage('Portrait', 3)}							
 					{this.getImage('Landscape', 7)}
-					{this.getImage('Portrait', 3)}					
+					{this.getImage('Portrait', 5)}					
+				</div>
+				<div className="landing__panel">
+					<div className="form">
+		        <div className="form__title">
+		          <h2>Welcome to BirdNerd</h2>
+		        </div>
+		        <div>
+		        	<p className="landing__intro">BirdNerd is a free photo sharing site for Birdwatchers. Within this site you can view photos, sighting locations and general bird information.</p>
+		        </div>
+		        <div className="landing__cta">
+		        	<p>Sign up to share your favourite birdwatching photos.</p>
+		        	<div className="landing__cta-btns">
+		        		<button className="nav-tabs__button" >
+		        			<Link to="/register">Sign up</Link>
+		        		</button>
+		        		<button className="nav-tabs__button" >
+		        			<Link to="/login">Login</Link>
+		        		</button>
+		        	</div>	
+		        </div>		        
+		        {/*
+		        <div className="form__submit">
+		        	<ul className="nav-tabs">
+							  <li className="tab-header-and-content">
+							    <button className="nav-tabs__button">Register</button>
+							  </li>
+							  <li className="tab-header-and-content">
+							    <button className="nav-tabs__button">Login</button>
+							  </li>
+						  </ul>
+		        </div> 		          						
+						*/}
+					</div>
 				</div>										
 			</div>
 		)
