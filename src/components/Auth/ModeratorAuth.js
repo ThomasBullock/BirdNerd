239f704ce3d6a3ed3afe5f86e30d3fdeb731bd42
Thomas Bullock <motbollox@gmail.com>
2018-01-21 14:25:03 +1100

    Added routerReducer so router is in state

--- src/components/Auth/ModeratorAuth.js
+++ src/components/Auth/ModeratorAuth.js
@@ -1,28 +1,30 @@
 import React, { Component }from 'react';
 import { connect } from 'react-redux';
-
+import { push } from 'react-router-redux';
+import store from '../../store';
 import history from '../../history';
 export default function(ComposedComponent) {
     class Authentication extends Component {
     
         componentWillMount() {
           if(!this.props.authenticated) {
-            history.push('/login');
+            console.log('not moderator!')
+            store.dispatch(push('/login')) // history.push('');
           } else {
               console.log('Role:============', this.props.role);
             if(this.props.role !== 'moderator') {
-                history.push('/bird');
+              store.dispatch(push('/bird')) // history.push('/bird');
             }
           }
         }
     
         componentWillUpdate(nextProps) {
           if(!nextProps.authenticated) {
-            history.push('/login');
+            store.dispatch(push('/login'));
           } else {
             console.log('Role:============', this.props.role);
             if(this.props.role !== 'user') {
-                history.push('/bird');
+              store.dispatch(push('/bird')) // history.push('/bird');
             }
           }
         }
